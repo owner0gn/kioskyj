8097cbf342b27fa854c5fb0f0f101a0e3df041a7
owner0gn <owner0gn@gmail.com>
2019-04-22 10:40:14 +0900

    init commit

---
+++ src/main/java/com/humantrion/KioskyjApplication.java
@@ -0,0 +1,13 @@
+package com.humantrion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KioskyjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KioskyjApplication.class, args);
+	}
+
+}
